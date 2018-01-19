0c249c4d66fa0a88ac8f5921bb270b05a8faf11f
Matthew Dworkin <mdworkin21@gmail.com>
2018-01-19 00:10:23 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -76,7 +76,25 @@ def move(index, player_token)
     end
   end
 
+  def turn_count
+     count = 0
+     @board.each do |turn|
+       if turn == "X" || turn == "O"
+         count+= 1
+       end
+     end
+     return count
+   end
 
+
+   def current_player
+     if turn_count % 2 != 0
+       return "O"
+     else
+       return "X"
+
+     end
+   end
 
 
 
