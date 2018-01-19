0889d8f366f672f5addd7966c1fcbc13301ec011
Matthew Dworkin <mdworkin21@gmail.com>
2018-01-19 00:06:07 +0000

    automatically backed up by learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,37 @@
+class TicTacToe
+
+  #InstnaceVariable
+def initialize
+  @board = board
+end
+
+
+
+#WIN_COMBINATIONS
+WIN_COMBINATIONS = [
+  [0, 1, 2], #top_row_win
+  [3, 4, 5], #middle_row_win
+  [6, 7, 8], #bottom_row_win
+  [0, 3, 6], #left_column_win
+  [1, 4, 7], #middle_column_win
+  [2, 5, 8], #right_column_win
+  [0, 4, 8], #left_diag_win
+  [6, 4, 2] #right_diag_win
+]
+
+
+#InstanceMethods
+
+
+
+
+
+
+
+
+
+
+
+
+end
+
